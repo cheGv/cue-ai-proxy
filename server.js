5c830b7534ce491710b53a07ea6fff5d085026a8
Guruvignesh <guruvignesh0033@gmail.com>
2026-04-19 01:23:37 +0530

    replace CORS with permissive wildcard

--- server.js
+++ server.js
@@ -8,19 +8,8 @@ const app = express();
 app.use(express.json({ limit: '2mb' }));
 
 app.use((req, res, next) => {
-  const origin = req.headers.origin || '';
-  const allowed = [
-    'https://trycueai.netlify.app',
-    'https://chegv.github.io',
-    'http://localhost:3000',
-    'http://localhost:8080',
-    'http://localhost:50625',
-    'http://localhost:5000',
-  ];
-  if (allowed.includes(origin) || origin.startsWith('http://localhost')) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
